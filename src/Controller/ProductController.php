0e3876807e3aea4dcd1c72e7886a8818b328e885
Sam QG <samqg01@gmail.com>
2023-02-22 14:51:26 +0100

    premier commit

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Form\ProductType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+/**
+ * 
+ *@Route("/product")
+ */
+class ProductController extends AbstractController
+{
+   /**
+   *@Route("/add", name="addProduct")
+   */
+   public function addProduct(Request $request, EntityManagerInterface $manager)
+   {
+
+    $product=new Product;
+
+    $form=$this->createForm(ProductType::class, $product);
+    $form->handleRequest($request);
+   
+   if ($form->isSubmitted() && $form->isValid())
+   {
+    $pictureFile=$form->get('picture')->getData();
+    // dd($pictureFile);
+
+    $picture_bdd=date("YmdHis").$pictureFile->getClientOriginalName();
+    // dd($picture_bdd);
+
+    try {
+
+        $pictureFile->move($this->getParameter('upload_directory'), $picture_bdd);
+
+
+    }catch (FileException $e){
+        dd($e);
+    }
+    $product->setPicture($picture_bdd);
+    // dd($product);
+    $manager->persist($product);
+    $manager->flush();
+
+    return $this->redirectToRoute("home");
+
+   }
+
+
+    return $this->render('product/addProduct.html.twig', [ 
+        'form'=>$form->createView()        
+
+    ]);
+     
+   }
+
+
+/**
+*@Route("/list", name="listProduct")
+*/
+public function listProduct()
+{
+
+ return $this->render('product/listProduct.html.twig', [ 
+
+    
+ ]);
+
+
+
+
+}
+
+
+}
